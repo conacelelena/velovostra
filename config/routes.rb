23806dcfab7d8854a3f6fcd7560edaea72032a4b
Cristina Conacel <conacelelena@users.noreply.github.com>
2025-01-14 15:20:02 +0000

    Added: dashboard page and dashboard layout.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "/dashboard" => "users#dashboard", as: :user_root
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -12,5 +13,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  root 'public#show'
+  root "public#show"
 end
